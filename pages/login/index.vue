7aa287a65a9dce8d799be44d613557b3df6c9056
Thuoq <thuonghamno3xz@gmail.com>
2022-09-10 15:47:18 +0700

    feat(Init): :tada: Init project

---
+++ pages/login/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login-register login-sidebar">
+    <div class="login-box card">
+      <div class="card-body">
+        <form class="form-horizontal form-material" @submit="onSubmit">
+          <h3 class="text-center m-b-20">Sign In</h3>
+          <div class="form-group">
+            <div class="col-xs-12">
+              <input
+                v-model="studentCode"
+                class="form-control"
+                type="text"
+                required
+                placeholder="Mã sinh viên"
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-xs-12">
+              <input
+                v-model="password"
+                class="form-control"
+                type="password"
+                required
+                placeholder="Mật khẩu"
+              />
+            </div>
+          </div>
+          <div class="form-group text-center">
+            <div class="col-xs-12 p-b-20">
+              <button
+                class="btn w-100 btn-lg btn-info btn-rounded text-white"
+                type="submit"
+              >
+                Log In
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      studentCode: '',
+      password: '',
+    }
+  },
+  watch: {
+    studentCode(newValue, oldValue) {
+      console.log(newValue, oldValue)
+    },
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(
+        `You are submit with studentCode ${this.studentCode}: ${this.password}`
+      )
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.login-register {
+  background-image: url('https://thanglong.edu.vn/sites/default/files/2020-05/facilities-top-01.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 100%;
+  width: 100%;
+  padding: 10% 0;
+  position: fixed;
+}
+.login-box {
+  width: 400px;
+  margin: 0px auto;
+
+  .footer {
+    width: 100%;
+    left: 0px;
+    right: 0px;
+  }
+}
+.login-sidebar {
+  padding: 0px;
+
+  margin-top: 0px;
+  .login-box {
+    right: 0px;
+    position: absolute;
+    height: 100%;
+  }
+}
+</style>
